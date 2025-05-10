9452db8b6f2bb0e16d2ed2620ca4c4fb4b603be3
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-10 17:15:15 +0300

    add app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Task manager</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
+        @vite(['resources/js/app.js'])
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Task manager</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="/">Task manager</a>
+                        <a class="nav-link" href="{{ route('tasks.index') }}">
+                        Tasks</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <h1>@yield('header')</h1>
+            <div>
+                @yield('content')
+            </div>
+        </div>
+
+    </body>
+</html>
