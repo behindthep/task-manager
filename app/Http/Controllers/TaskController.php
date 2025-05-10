52fa75c4f35388512c5db940b18be262bf36d26a
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-10 16:54:53 +0300

    add TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Requests\Task\{StoreRequest, UpdateRequest};
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $tasks = $request->get('name')
+            ? Task::where('name', 'like', "%{$request->get('name')}%")->paginate() 
+            : Task::paginate();
+        $inputName = $request->input('name');
+        return view('task.index', compact('tasks', 'inputName'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $task = new Task();
+        return view('task.create', compact('task'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $task = new Task($data);
+        $task->save();
+        return redirect()->route('task.index')->with('success', 'Task created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return view('task.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        return view('task.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        
+        $task-> fill($data);
+        $task->save();
+        return redirect()->route('task.index')->with('success', 'Task updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('task.index')->with('success', 'Task removed successfully');
+    }
+}
