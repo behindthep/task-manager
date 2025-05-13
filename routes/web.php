874528ab56ba22c0e4077995bfeb20356bb190d8
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-13 19:36:56 +0300

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/', 'dashboard')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
