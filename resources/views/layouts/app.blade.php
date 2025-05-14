dbda699c1728bcd4617cbe9be6a6f2563d41983e
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-14 15:49:54 +0300

    update layout app.balde.php
    CSRF — тип атаки, при которой злоумышленник может заставить пользователя
    выполнить нежелательное действие на веб-сайте в котором пользователь
    аутентифицирован.
    
    CSRF-токены для защиты - при отправке формы, Laravel проверяет, что
    запрос содержит действительный CSRF-токен, сгенерированный на сервере.
    Убедиться, что запрос исходит от аутентифицированного пользователя и не
    был подделан.
    
    Указываем, что параметр CSRF-токена в запросах будет называться _token.
    Автоматически добавляться CSRF-токен в заголовки запросов.
    Коду на клиентской стороне находить и использовать этот токен при
    отправке AJAX-запросов.
    
    AJAX (Asynchronous JavaScript and XML) — технология, позволяет
    веб-страницам асинхронно обмениваться данными с сервером без
    перезагрузки страницы. Обновлять только необходимые части страницы,
    без полной перезагрузки.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
 
         <title>{{ $title }}</title>
 
@@ -24,6 +25,7 @@
 
             <!-- Page Content -->
             <main class="max-w-screen-xl px-4 pt-20 pb-8 mx-auto">
+                @include('flash::message')
                 {{ $slot }}
             </main>
         </div>
