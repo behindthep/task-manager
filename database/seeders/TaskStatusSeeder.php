dba555d0d59fd2a66e153ffbea9082c116a52418
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-14 13:15:48 +0300

    add TaskStatus Factory and Seeder
    - Factory для генерирации экземпляров модели с
    фейковыми значениями полей. Используются внутри
    Seeders, тестов или в коде, где нужно создать
    экземпляры модели.
    - Seeder для заполнения базы начальными данными.
    Регестрировать их в DatabaseSeeder.
    
    1) В Factory определяем каким будет экзмепляр
    модели созданный через неё. Наследуем генератор
    фейковых данных из class Factory ($this).
    2) В Seeder создаём экзампляры модели с
    определёнными данными, используя фабрику и
    заполняем ими базу - в DatabaseSeeder вызываем
    сидеры.

---
+++ database/seeders/TaskStatusSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\TaskStatus;
+
+class TaskStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TaskStatus::factory()->createMany([
+            ['name' => 'новый'],
+            ['name' => 'в работе'],
+            ['name' => 'на тестировании'],
+            ['name' => 'завершен'],
+        ]);
+    }
+}
