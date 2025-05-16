d4002a3f4ee619fa803d1b51fe6a2bf23740f979
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-16 11:14:46 +0300

    update Label & TaskStatus Models
    - В TaskStatus метод tasks() устанавливает связь "один ко многим" с
    моделью Task. hasMany - экземпляр TaskStatus может иметь много
    связанных Tasks.
    
    'status_id' - внешний ключ в таблице tasks, ссылается на id в таблице
    task_statuses.
    
    - В Label метод tasks() устанавливает связь "многие ко многим" с
    моделью Task. belongsToMany - экземпляр Label может быть связан
    с несколькими Tasks, и одна Task может иметь несколько Labels.
    
    Связь требует промежуточную таблицу label_task, которая содержит
    label_id и task_id.

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Label extends Model
 {
-    /** @use HasFactory<\Database\Factories\LabelFactory> */
     use HasFactory;
 
     protected $fillable = [
         'name',
         'description',
     ];
+
+    public function tasks(): BelongsToMany
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
