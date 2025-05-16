f4bbc574810e6b26becde4d737c73e60a631fc3d
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-05-16 22:14:29 +0300

    add Task and User seeders
    attach добавляет новые метки

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            UserSeeder::class,
             TaskStatusSeeder::class,
             LabelSeeder::class,
+            TaskSeeder::class
         ]);
     }
 }
