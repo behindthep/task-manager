59cb84fecc7347b17d3fed88f90bb68ebdd79467
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-07-03 22:21:11 +0300

    return breeze profile

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,16 @@ use App\Http\Controllers\{
 
 Route::view('/', 'dashboard')->name('dashboard');
 
-Route::view('/docs', 'scribe.index')->name('docs');
-
 Route::resources([
     'task_statuses' => TaskStatusController::class,
     'labels' => LabelController::class,
     'tasks' => TaskController::class
 ]);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
