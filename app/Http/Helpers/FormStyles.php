14e62b8ac1ff0813f4bcc401ec4c300be8496c40
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-07-09 14:31:02 +0300

    add helper for forms views

--- app/Http/Helpers/FormStyles.php
+++ app/Http/Helpers/FormStyles.php
@@ -4,7 +4,7 @@ namespace App\Http\Helpers;
 
 class FormStyles
 {
-    public static function commonClasses(): string
+    public static function inputField(): string
     {
         return 'rounded border-gray-300 w-full mt-1 mb-3';
     }
