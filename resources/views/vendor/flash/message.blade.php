d7c83b977e98f8d42a02994c346a8fbcd7244ecd
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-07-10 22:50:49 +0300

    fix render deploy

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -5,15 +5,15 @@
                 <div class="flex-shrink-0">
                     <!-- Иконка для успеха -->
                     @if ($flash['level'] === 'success')
-                        <svg class="w-5 h-5 text-green-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5 text-green-600" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-11.293l-3.293 3.293a1 1 0 001.414 1.414L10 9.414l2.879 2.879a1 1 0 001.414-1.414L10 6.707z" clip-rule="evenodd" />
                         </svg>
                     @elseif ($flash['level'] === 'warning')
-                        <svg class="w-5 h-5 text-yellow-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5 text-yellow-600" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-11.293l-3.293 3.293a1 1 0 001.414 1.414L10 9.414l2.879 2.879a1 1 0 001.414-1.414L10 6.707z" clip-rule="evenodd" />
                         </svg>
                     @elseif ($flash['level'] === 'error')
-                        <svg class="w-5 h-5 text-red-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5 text-red-600" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-11.293l-3.293 3.293a1 1 0 001.414 1.414L10 9.414l2.879 2.879a1 1 0 001.414-1.414L10 6.707z" clip-rule="evenodd" />
                         </svg>
                     @endif
