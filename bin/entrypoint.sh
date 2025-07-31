754684e067a9092db5096164cb882f77aaebf3df
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-07-31 22:49:50 +0300

    fix entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,15 +2,14 @@
 
 set -e
 
+php artisan migrate --force
+
 USER_COUNT=$(php artisan tinker --execute="echo \App\Models\User::count();")
 
 if [ "$USER_COUNT" -eq 0 ]; then
   php artisan migrate:fresh --force
-  echo "No data in tables, running seeders."
+  echo "No data in tables, running seeders..."
   php artisan db:seed --force
-else
-  php artisan migrate --force
-  echo "Users table has data, skipping seeders."
 fi
 
 php artisan serve --host=0.0.0.0 --port=$PORT
