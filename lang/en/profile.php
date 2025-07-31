1ce942a2e701777523e828602e61d28d4f02f558
Alexey Kolvakh <al.kolv19@gmail.com>
2025-07-31 22:18:32 +0300

    add lang records for profile

---
+++ lang/en/profile.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'info' => 'Profile Information',
+    'updated' => 'Profile successfully updated',
+    'deleted' => 'Profile successfully deleted',
+    'deletion_confirm' => 'Are you sure you want to delete your account?',
+    'deletion_info' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+    'deletion_confirm_description' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.',
+    'delete' => 'Delete Account',
+    'update_info' => 'Update your account\'s profile information and email address.',
+    'current_password' => 'Current Password',
+    'new_password' => 'New Password',
+    'сonfirm_password' => 'Confirm Password',
+    'update_password' => 'Update Password',
+    'update_password_info' => 'Ensure your account is using a long, random password to stay secure.',
+    'save' => 'Save',
+    'saved' => 'Saved',
+    'cancel' => 'Cancel',
+    'unverified_email' => 'Your email address is unverified.',
+    'sent_verification_email' => 'Click here to re-send the verification email.',
+    'info_verification_email' => 'A new verification link has been sent to your email address.',
+];
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+            {{ __('profile.info') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __('profile.update_info') }}
         </p>
     </header>
 
@@ -29,35 +29,34 @@
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+            <div>
+                <p class="text-sm mt-2 text-gray-800">
+                    {{ __('profile.unverified_email') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
+                    <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('profile.sent_verification_email') }}
+                    </button>
+                </p>
 
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
+                @if (session('status') === 'verification-link-sent')
+                <p class="mt-2 font-medium text-sm text-green-600">
+                    {{ __('profile.info_verification_email') }}
+                </p>
+                @endif
+            </div>
             @endif
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ __('profile.save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+            <p
+                x-data="{ show: true }"
+                x-show="show"
+                x-transition
+                x-init="setTimeout(() => show = false, 2000)"
+                class="text-sm text-gray-600">{{ __('profile.saved') }}</p>
             @endif
         </div>
     </form>
